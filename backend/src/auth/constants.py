81c63d54ff4c3c8e6467e7c8ecbfe2340d70a623
Radoslaw Kuzicki <rkuzicki@gmail.com>
2023-03-14 14:40:19 +0100

    remove useless files

--- backend/src/auth/constants.py
+++
@@ -1,8 +0,0 @@
-MINIMUM_USERNAME_LENGTH = 5
-MINIMUM_PASSWORD_LENGTH = 8
-
-STRONG_PASSOWRD_REGEX = (
-    "(?=^.{"
-    + str(MINIMUM_PASSWORD_LENGTH)
-    + ",}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-)
