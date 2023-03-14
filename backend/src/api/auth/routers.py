eadfa7d54e502d5f1729e49c59e06804e1e78423
Radoslaw Kuzicki <rkuzicki@gmail.com>
2023-03-14 14:53:36 +0100

    Change auth endpoint

--- backend/src/api/auth/routers.py
+++ backend/src/api/auth/routers.py
@@ -4,6 +4,6 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 
-@router.get("/register", status_code=201)
+@router.post("/users", status_code=201)
 async def register():
     return JSONResponse({"you_are": "registered"})
