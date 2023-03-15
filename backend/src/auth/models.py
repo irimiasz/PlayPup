82f0ed83767f8d576a57dfd61fda40ee0fc0f044
Radoslaw Kuzicki <rkuzicki@gmail.com>
2023-03-15 14:56:29 +0100

    Add comment about fields from fastapi-users on User model

--- backend/src/auth/models.py
+++ backend/src/auth/models.py
@@ -6,5 +6,13 @@ from src.database import Base
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "users"
+    """
+    Fields inherited from SQLAlchemyBaseUserTableUUID:
+    - id: UUID
+    - email: str
+    - hashed_password: str
+    - is_active: bool
+    - is_superuser: bool
+    """
 
     username = Column(String)
